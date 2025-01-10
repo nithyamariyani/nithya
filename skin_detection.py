17b8f513d8b1546a9dd4781ca0091025f8d4d372
nithyamariyani <151911453+nithyamariyani@users.noreply.github.com>
2025-01-10 22:23:38 +0530

    skin

--- skin_detection.py
+++ skin_detection.py
@@ -17,45 +17,16 @@ def skin_detection(img_path):
     skin_cluster_row, skin_cluster_label = skin_cluster(dframe)
     cluster_label_mat = cluster_matrix(
         dframe, dframe_removed, skin_cluster_label, height, width)
-    # display_all_images(images)
-    # final_segment(images, cluster_label_mat)
+    final_segment(images, cluster_label_mat)
+    display_all_images(images)
+    # write_all_images(images)
+    skin_cluster_row = np.delete(skin_cluster_row, 1)
+    skin_cluster_row = np.delete(skin_cluster_row, 2)
     return np.delete(skin_cluster_row, -1)
+    # return images["final_segment"]
 
-# display an image plus label and wait for key press to continue
-
-
-def display_image(image, name):
-    window_name = name
-    cv2.namedWindow(window_name)
-    cv2.imshow(window_name, image)
-    cv2.waitKey()
-    cv2.destroyAllWindows()
-
-# segment using otsu binarization and thresholding
-
-
-def thresholding(images):
-    histogram, bin_edges = np.histogram(
-        images["grayscale"].ravel(), 256, [0, 256])
-    Totsu, threshold_image_otsu = cv2.threshold(
-        images["grayscale"], 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    Tmax = np.where(histogram[:] == max(histogram[:]))[0][0]
-    Tfinal = round((Tmax + Totsu)/2) if Tmax > 10 else round((Tmax + Totsu)/4)
-
-    # plot_histogram(histogram, bin_edges, Totsu, Tmax, Tfinal)
-
-    threshold_type = (cv2.THRESH_BINARY if Tmax <
-                      220 else cv2.THRESH_BINARY_INV)
-    Tfinal, threshold_image = cv2.threshold(
-        images["grayscale"], Tfinal, 255, threshold_type)
-
-    masked_img = cv2.bitwise_and(
-        images["BGR"], images["BGR"], mask=threshold_image)
-    return masked_img
 
 # Plot Histogram and Threshold values
-
-
 def plot_histogram(histogram, bin_edges, Totsu, Tmax, Tfinal):
     plt.figure()
     plt.title("Image Histogram")
@@ -69,6 +40,16 @@ def plot_histogram(histogram, bin_edges, Totsu, Tmax, Tfinal):
     plt.legend()
     plt.show()
 
+# display an image plus label and wait for key press to continue
+
+
+def display_image(image, name):
+    window_name = name
+    cv2.namedWindow(window_name)
+    cv2.imshow(window_name, image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
 # Display all images
 
 
@@ -76,18 +57,52 @@ def display_all_images(images):
     for key, value in images.items():
         display_image(value, key)
 
+# write all images
+
+
+def write_all_images(images):
+    for key, value in images.items():
+        cv2.imwrite(key+'.jpg', value)
+
 # read in image into openCV
 
 
 def read_image(dir):
+    maxwidth, maxheight = 400, 500
     image_path = dir
     img_BGR = cv2.imread(image_path, 3)
-    img_BGR = cv2.resize(img_BGR, (375, 500))
+    f1 = maxwidth / img_BGR.shape[1]
+    f2 = maxheight / img_BGR.shape[0]
+    f = min(f1, f2)  # resizing factor
+    dim = (int(img_BGR.shape[1] * f), int(img_BGR.shape[0] * f))
+    img_BGR = cv2.resize(img_BGR, dim)
+    # img_BGR = cv2.resize(img_BGR, (375, 500))
     return img_BGR
 
+# segment using otsu binarization and thresholding
+
+
+def thresholding(images):
+    histogram, bin_edges = np.histogram(
+        images["grayscale"].ravel(), 256, [0, 256])
+    Totsu, threshold_image_otsu = cv2.threshold(
+        images["grayscale"], 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    Tmax = np.where(histogram[:] == max(histogram[:]))[0][0]
+    Tfinal = round((Tmax + Totsu)/2) if Tmax > 10 else round((Tmax + Totsu)/4)
+
+    plot_histogram(histogram, bin_edges, Totsu, Tmax, Tfinal)
+
+    threshold_type = (cv2.THRESH_BINARY if Tmax <
+                      220 else cv2.THRESH_BINARY_INV)
+    Tfinal, threshold_image = cv2.threshold(
+        images["grayscale"], Tfinal, 255, threshold_type)
+
+    masked_img = cv2.bitwise_and(
+        images["BGR"], images["BGR"], mask=threshold_image)
+    return masked_img
+
+
 # Grayscle and Thresholding and HSV & YCrCb color space conversions
-
-
 def image_conversions(img_BGR):
     images = {
         "BGR": img_BGR,
@@ -98,13 +113,15 @@ def image_conversions(img_BGR):
         images["thresholded"], cv2.COLOR_BGR2HSV)
     images["YCrCb"] = cv2.cvtColor(
         images["thresholded"], cv2.COLOR_BGR2YCrCb)
+    # display_all_images(images)
     return images
 
 
 # Predict skin pixels
 def skin_predict(images):
     height, width = images["grayscale"].shape
-    images["skin_predict"] = images["grayscale"]
+    images["skin_predict"] = np.empty_like(images["grayscale"])
+    images["skin_predict"][:] = images["grayscale"]
 
     for i in range(height):
         for j in range(width):
@@ -122,10 +139,10 @@ def dataframe(images):
     dframe['H'] = images["HSV"].reshape([-1, 3])[:, 0]
 
     # Getting the y-x coordintated
-    # gray = cv2.cvtColor(images["thresholded"], cv2.COLOR_BGR2GRAY)
-    # yx_coords = np.column_stack(np.where(gray >= 0))
-    # dframe['Y'] = yx_coords[:, 0]
-    # dframe['X'] = yx_coords[:, 1]
+    gray = cv2.cvtColor(images["thresholded"], cv2.COLOR_BGR2GRAY)
+    yx_coords = np.column_stack(np.where(gray >= 0))
+    dframe['Y'] = yx_coords[:, 0]
+    dframe['X'] = yx_coords[:, 1]
 
     dframe['Cr'] = images["YCrCb"].reshape([-1, 3])[:, 1]
     dframe['Cb'] = images["YCrCb"].reshape([-1, 3])[:, 2]
@@ -164,18 +181,12 @@ def skin_cluster(dframe):
 
 # Append removed pixels to the dataframe and get cluster matrix
 def cluster_matrix(dframe, dframe_removed, skin_cluster_label, height, width):
-    # Ensure 'cluster' column is added to dframe_removed
-    dframe_removed['cluster'] = -1
-
-    # Concatenate the two DataFrames
-    dframe = pd.concat([dframe, dframe_removed], ignore_index=False).sort_index()
-
-    # Update 'cluster' column based on skin_cluster_label
-    dframe['cluster'] = (dframe['cluster'] == skin_cluster_label).astype(int) * 255
-
-    # Reshape to the required matrix form
-    cluster_label_mat = np.asarray(dframe['cluster'].values).reshape(height, width).astype(np.uint8)
-    
+    dframe_removed['cluster'] = np.full((len(dframe_removed.index), 1), -1)
+    dframe = dframe.append(dframe_removed, ignore_index=False).sort_index()
+    dframe['cluster'] = (dframe['cluster'] ==
+                         skin_cluster_label).astype(int) * 255
+    cluster_label_mat = np.asarray(
+        dframe['cluster'].values.reshape(height, width), dtype=np.uint8)
     return cluster_label_mat
 
 # final segmentation
@@ -184,7 +195,8 @@ def cluster_matrix(dframe, dframe_removed, skin_cluster_label, height, width):
 def final_segment(images, cluster_label_mat):
     final_segment_img = cv2.bitwise_and(
         images["BGR"], images["BGR"], mask=cluster_label_mat)
-    display_image(final_segment_img, "final segmentation")
+    images["final_segment"] = final_segment_img
+    # display_image(final_segment_img, "final segmentation")
 
 
 # print(skin_detection("images\Optimized-selfieNig-cropped.jpg"))
