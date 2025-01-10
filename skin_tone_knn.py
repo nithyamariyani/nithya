b89796b44c2958c66529d342ec72334051332dcc
nithyamariyani <151911453+nithyamariyani@users.noreply.github.com>
2025-01-10 22:24:08 +0530

    skin

--- skin_tone_knn.py
+++ skin_tone_knn.py
@@ -2,19 +2,19 @@
 To classify the input skin into one of the 6 skin tones
 """
 import pandas as pd
-import os
 from sklearn.neighbors import KNeighborsClassifier
-from models.skin_tone.skin_detection import skin_detection
 
-def identify_skin_tone(image_path, dataset):
-    mean_color_values = skin_detection(image_path)
-    df = pd.read_csv(dataset)
+
+def skin_tone_knn(mean_values):
+    # df = pd.read_csv("public\pre-processing\skin_tone_dataset_RGB.csv")
+    df = pd.read_csv("public\skin_tone_dataset.csv")
+
     X = df.iloc[:, [1, 2, 3]].values
     y = df.iloc[:, 0].values
 
     classifier = KNeighborsClassifier(n_neighbors=6, metric='minkowski', p=2)
     classifier.fit(X, y)
 
-    X_test = [mean_color_values]
+    X_test = [mean_values]
     y_pred = classifier.predict(X_test)
     return y_pred[0]
